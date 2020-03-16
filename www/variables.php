fa588663db55f1a30757868634429c925695c38c
Arjun Nair <nair.arjun47@yahoo.com>
2020-03-15 23:43:55 -0700

    Adding variables file

---
+++ www/variables.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Variables</title>
+</head>
+<body>
+
+    <?php 
+        echo "Roses are red <br>";
+        echo "Violets are blue <br>";
+        echo "Sugar is sweet <br>";
+        echo "Angelina is too <br>";
+    ?>
+    
+</body>
+</html>
